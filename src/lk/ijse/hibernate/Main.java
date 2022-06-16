3294dca510633a6d5e70bf967399b21326812ac4
unknown <sohaniabeywickrama@gmail.com>
2022-06-16 11:14:44 +0530

    create customer

--- src/lk/ijse/hibernate/Main.java
+++ src/lk/ijse/hibernate/Main.java
@@ -9,7 +9,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Customer customer1 = new Customer("C001", "sohani", "Panadura", "076 1236545");
+        Customer customer2 = new Customer("C002", "sandali", "colombo", "076 2546321");
+        Customer customer3 = new Customer("C003", "abeywickrama", " kaluthara", "076 2587463");
 
         Session session = FactoryConfiguration.getInstance().getSession();
 
